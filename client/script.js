231cc82be9380e487de1821123c54eb4eba7cee1
mbrndt <mareike.brandt@hotmail.de>
2023-01-16 15:47:25 +0900

    feat: able to send messages in UI, bot answers with thinking

--- client/script.js
+++ client/script.js
@@ -1,7 +1,7 @@
 import bot from "./assets/bot.svg";
 import user from "./assets/user.svg";
 
-const from = document.querySelector("from");
+const form = document.querySelector("form");
 const chatContainer = document.querySelector("#chat_container");
 
 let loadInterval;
@@ -38,3 +38,44 @@ function generateUniqueId() {
 
 	return `id-${timestamp}-${hexadecimalString}`;
 }
+
+function chatStripe(isAi, value, uniqueId) {
+	return `
+		<div class="wrapper ${isAi && "ai"}">
+		<div class="chat">
+		<div class="profile">
+		<img src="${isAi ? bot : user}" alt="${isAi ? "bot" : "user"}" />
+		</div>
+		<div class="message" id=${uniqueId}>${value}</dvc>
+		</div>
+		</div>
+		`;
+}
+
+const handleSubmit = async (e) => {
+	e.preventDefault();
+
+	const data = new FormData(form);
+
+	// users chatstripe
+	chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
+
+	form.reset();
+
+	// bots chatstripe
+	const uniqueId = generateUniqueId();
+	chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+
+	chatContainer.scrollTop = chatContainer.scrollHeight;
+
+	const messageDiv = document.getElementById(uniqueId);
+
+	loader(messageDiv);
+};
+
+form.addEventListener("submit", handleSubmit);
+form.addEventListener("keyup", (e) => {
+	if (e.keyCode === 13) {
+		handleSubmit(e);
+	}
+});
